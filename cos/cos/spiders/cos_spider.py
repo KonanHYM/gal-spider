ead0a2b3f7277a62654802c8010d1e1be93b2948
黄一鸣 <724417787@qq.com>
2016-01-26 17:17:33 +0800

    modify cos spider

--- cos/cos/spiders/cos_spider.py
+++ cos/cos/spiders/cos_spider.py
@@ -19,19 +19,23 @@ class CosSpider(scrapy.spiders.Spider):
         page_selector = HtmlXPathSelector(response)
         url = response.url
         log.msg(url)
-        print page_selector.select("//div[@class='pagination-default clearfix']/a/text()")
-        if page_selector.select("//div[@class='pagination-default clearfix']/a/text()").extract() == u'下一页':
-            next_page_url = page_selector.select("//div[@class='pagination-default clearfix']/a/@href").extract()[0]
-            log.msg(next_page_url)
-            #TODO:加入一层redis缓存 获取没有爬取过的列表页 进去下一页
-            yield scrapy.Request(next_page_url, callback=self.parse)
+
+        next_page_url = page_selector.select("//div[@class='pagination-default clearfix']/a/@href").extract()[-2]
+        log.msg(next_page_url)
+        #TODO:加入一层redis缓存 获取没有爬取过的列表页 进去下一页
+        yield scrapy.Request(next_page_url, callback=self.parse)
 
         #TODO:解析当前页
-        invitation_list = 'http://tieba.baidu.com' + page_selector.select("//div[@class='threadlist_title pull_left j_th_tit ']/a/@href").extract()[0]
+        invitation_list = page_selector.select("//div[@class='threadlist_title pull_left j_th_tit ']/a/@href").extract()
         for invitation_url in invitation_list:
-            yield scrapy.Request(invitation_list, callback=self.parseCospics)
+            invitation_url = 'http://tieba.baidu.com' + invitation_url
+            log.msg(invitation_url)
+            yield scrapy.Request(invitation_url, callback=self.parseCospics)
 
     #获取图片 进入Pipeline 存储图片
     def parseCospics(self, response):
         page_selector = HtmlXPathSelector(response)
         print response.url
+        img_list = page_selector.select('//img/@src')
+        for img in img_list:
+            print img
